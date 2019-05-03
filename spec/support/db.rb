ea46f63d85d61033feccdd05414478670ab1e981
BDCraven <bencraven@hotmail.com>
2019-05-03 14:33:48 +0100

    Implement storage layer and add passing integration specs

---
+++ spec/support/db.rb
@@ -0,0 +1,12 @@
+# runs after all specs have been loaded but before the first one actually runs
+RSpec.configure do |c|
+  c.before(:suite) do
+    Sequel.extension :migration
+    Sequel::Migrator.run(DB, 'db/migrations')
+    DB[:expenses].truncate
+  end
+
+  c.around(:example, :db) do |example|
+    DB.transaction(rollback: :always) { example.run }
+  end
+end
